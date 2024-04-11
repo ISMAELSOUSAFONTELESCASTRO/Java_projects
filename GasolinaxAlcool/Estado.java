c168a0c8b8c2268d0850af0c39dc1404572c7b9d
Ismael Sousa <165102646+ISMAELSOUSAFONTELESCASTRO@users.noreply.github.com>
2024-04-11 15:09:04 -0300

    Update Estado.java

--- GasolinaxAlcool/Estado.java
+++ GasolinaxAlcool/Estado.java
@@ -1,7 +1,9 @@
 public class Estado {
+    
     private String Nome;
     private String Sigla;
     private double VMAlcool;
+    private double VMGasolina;
 
     public String getNome() {
         return Nome;
@@ -11,6 +13,9 @@ public class Estado {
     }
     public double getVMAlcool() {
         return VMAlcool;
+    }
+    public double getVMGasolina() {
+        return VMGasolina;
     }
 
 
@@ -23,6 +28,15 @@ public class Estado {
     public void setVMAlcool(double vMAlcool) {
         this.VMAlcool = vMAlcool;
     }
+    public void setVMGasolina(double vMGasolina) {
+        VMGasolina = vMGasolina;
+    }
 
-    
-}
+    public String MaisVantajoso(){
+        if(this.VMAlcool >= 0.7*this.VMGasolina){
+            return this.Nome + " " + this.Sigla + "\n" + "Alcool";
+        }
+        else{
+            return this.Nome + " " + this.Sigla + "\n" + "Alcool";
+        }
+    }
