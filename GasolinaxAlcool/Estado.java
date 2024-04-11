a7e24083d5021105b6343717f8d3452f374059e1
Ismael Sousa <165102646+ISMAELSOUSAFONTELESCASTRO@users.noreply.github.com>
2024-04-11 12:33:44 -0300

    Estado.java

---
+++ GasolinaxAlcool/Estado.java
@@ -0,0 +1,28 @@
+public class Estado {
+    private String Nome;
+    private String Sigla;
+    private double VMAlcool;
+
+    public String getNome() {
+        return Nome;
+    }
+    public String getSigla() {
+        return Sigla;
+    }
+    public double getVMAlcool() {
+        return VMAlcool;
+    }
+
+
+    public void setNome(String nome) {
+        this.Nome = nome;
+    }
+    public void setSigla(String sigla) {
+        this.Sigla = sigla;
+    }
+    public void setVMAlcool(double vMAlcool) {
+        this.VMAlcool = vMAlcool;
+    }
+
+    
+}
