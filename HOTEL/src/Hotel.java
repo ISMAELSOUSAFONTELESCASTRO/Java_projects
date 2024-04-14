58b47523d23c230265ffa0d587f722d2f3b62b11
Ismael Sousa <ismaelsousafcastro@gmail.com>
2024-04-14 13:02:44 -0300

    updates

--- HOTEL/src/Hotel.java
+++ HOTEL/src/Hotel.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 public class Hotel {
     private double diaria;
     private boolean cfdm;
-    private ArrayList<Integer> qvagos = new ArrayList<>();
+    private ArrayList<Quarto> qvagos = new ArrayList<>();
     private float nota;
 
-    public Hotel(double diaria, boolean cfdm, ArrayList<Integer> qvagos, float nota){
+    public Hotel(double diaria, boolean cfdm, ArrayList<Quarto> qvagos, float nota){
         this.diaria = diaria;
         this.cfdm = cfdm;
         for(int i = 0; i < qvagos.size(); i++){
@@ -13,4 +13,45 @@ public class Hotel {
         }
         this.nota = nota;
     }
+
+    public double getDiaria() {
+        return this.diaria;
+    }
+    public float getNota() {
+        return this.nota;
+    }
+    public ArrayList<Quarto> getQvagos() {
+        return this.qvagos;
+    }
+    public boolean getCfdm(){
+        return this.cfdm;
+    }
+
+
+    public void setCfdm(boolean cfdm) {
+        this.cfdm = cfdm;
+    }
+    public void setDiaria(double diaria) {
+        this.diaria = diaria;
+    }
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+    public void setQvagos(ArrayList<Quarto> qvagos) {
+        for(int i = 0; i < qvagos.size(); i++){
+            this.qvagos.add(qvagos.get(i));
+        }
+    }
+
+    public int ocuparQuarto(int quarto){
+        for(int i = 0; i < this.qvagos.size();i++){
+            if(this.qvagos.get(i).getNumero() == quarto){
+                
+            }
+        }
+    }
+
+
+
+
 }
