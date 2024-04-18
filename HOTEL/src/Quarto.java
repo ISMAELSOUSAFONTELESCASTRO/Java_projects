7526dfcc589e87c8d253fea128870a8f4099d141
Ismael Sousa <ismaelsousafcastro@gmail.com>
2024-04-18 15:18:52 -0300

    update

--- HOTEL/src/Quarto.java
+++ HOTEL/src/Quarto.java
@@ -1,3 +1,41 @@
 public class Quarto {
+    private int numero;
+    private int dias;
+
+    public Quarto(int numero, int dias){
+        this.numero = numero;
+        this.dias = dias;
+    }
+    public Quarto(int numero){
+        this.numero = numero;
+    }
+
+    public int getDias() {
+        return dias;
+    }
+    public int getNumero() {
+        return numero;
+    }
+
+
+    public void setDias(int dias) {
+        this.dias = dias;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    void subDias(){
+        this.dias -=1;
+        if(this.dias < 0){
+            this.dias = 0;
+        }
+    }
+
+    void zerarDias(){
+        this.dias = 0;
+    }
+
+
 
 }
